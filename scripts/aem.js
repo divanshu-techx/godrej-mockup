f3ecf1e1ddb899c9f485a882a2d1e5d109dc1ac0
dgoyal@hyperpointe.com <divanshu.goyal@techx-consulting.com>
2024-05-13 17:42:28 +0530

    commented optimized picture

--- scripts/aem.js
+++ scripts/aem.js
@@ -318,44 +318,44 @@ function getMetadata(name, doc = document) {
  * @param {Array} [breakpoints] Breakpoints and corresponding params (eg. width)
  * @returns {Element} The picture element
  */
-function createOptimizedPicture(
-  src,
-  alt = '',
-  eager = false,
-  breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
-) {
-  const url = new URL(src, window.location.href);
-  const picture = document.createElement('picture');
-  const { pathname } = url;
-  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+// function createOptimizedPicture(
+//   src,
+//   alt = '',
+//   eager = false,
+//   breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
+// ) {
+//   const url = new URL(src, window.location.href);
+//   const picture = document.createElement('picture');
+//   const { pathname } = url;
+//   const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
 
-  // webp
-  breakpoints.forEach((br) => {
-    const source = document.createElement('source');
-    if (br.media) source.setAttribute('media', br.media);
-    source.setAttribute('type', 'image/webp');
-    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
-    picture.appendChild(source);
-  });
+//   // webp
+//   breakpoints.forEach((br) => {
+//     const source = document.createElement('source');
+//     if (br.media) source.setAttribute('media', br.media);
+//     source.setAttribute('type', 'image/webp');
+//     source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+//     picture.appendChild(source);
+//   });
 
-  // fallback
-  breakpoints.forEach((br, i) => {
-    if (i < breakpoints.length - 1) {
-      const source = document.createElement('source');
-      if (br.media) source.setAttribute('media', br.media);
-      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
-      picture.appendChild(source);
-    } else {
-      const img = document.createElement('img');
-      img.setAttribute('loading', eager ? 'eager' : 'lazy');
-      img.setAttribute('alt', alt);
-      picture.appendChild(img);
-      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
-    }
-  });
+//   // fallback
+//   breakpoints.forEach((br, i) => {
+//     if (i < breakpoints.length - 1) {
+//       const source = document.createElement('source');
+//       if (br.media) source.setAttribute('media', br.media);
+//       source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+//       picture.appendChild(source);
+//     } else {
+//       const img = document.createElement('img');
+//       img.setAttribute('loading', eager ? 'eager' : 'lazy');
+//       img.setAttribute('alt', alt);
+//       picture.appendChild(img);
+//       img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+//     }
+//   });
 
-  return picture;
-}
+//   return picture;
+// }
 
 /**
  * Set template (page structure) and theme (page styles).
